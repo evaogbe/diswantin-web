1eb7c3c300823e3606249e7b48ac49668ea8c5ed
Eva Ogbe <eoogbe@gmail.com>
2024-11-18 12:01:05 -0500

    🩺 Use host from env var

--- app/system.server/env.ts
+++ app/system.server/env.ts
@@ -4,6 +4,7 @@ import * as v from "valibot";
 const envSchema = v.object({
   APP_ENV: v.picklist(["production", "development", "test"]),
   DATABASE_URL: v.pipe(v.string(), v.url()),
+  PUBLIC_HOST: v.pipe(v.string(), v.url()),
 });
 
 export const env = v.parse(envSchema, process.env);
