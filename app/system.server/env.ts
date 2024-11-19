a7b9624fef25e37709798bec14024ce9162390c8
Eva Ogbe <eoogbe@gmail.com>
2024-11-19 15:32:45 -0500

    🦺 Loosen HOST validation (#11)

--- app/system.server/env.ts
+++ app/system.server/env.ts
@@ -7,7 +7,7 @@ const envSchema = v.object({
     "development",
   ),
   DATABASE_URL: v.pipe(v.string(), v.url()),
-  HOST: v.optional(v.pipe(v.string(), v.url())),
+  HOST: v.optional(v.string()),
 });
 
 export const env = v.parse(envSchema, process.env);
