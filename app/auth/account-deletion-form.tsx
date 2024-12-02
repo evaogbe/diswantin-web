64c1381882b6b5efb2a4b44421888622f4b28720
Eva Ogbe <eoogbe@gmail.com>
2024-12-01 19:11:12 -0500

    ♻️ Extract AccountDeletionForm component (#55)

---
+++ app/auth/account-deletion-form.tsx
@@ -0,0 +1,134 @@
+import { FormProvider, useForm } from "@conform-to/react";
+import type { SubmissionResult } from "@conform-to/react";
+import { Form, Link } from "@remix-run/react";
+import { getValibotConstraint, parseWithValibot } from "conform-to-valibot";
+import { AlertCircle, X } from "lucide-react";
+import { useId, useState } from "react";
+import { AuthenticityTokenInput } from "remix-utils/csrf/react";
+import { uid } from "uid";
+import { accountDeletionSchema } from "./model";
+import { FormField, FormItem, FormLabel, FormMessage } from "~/form/field";
+import { Input } from "~/form/input";
+import { Alert, AlertTitle, AlertDescription } from "~/ui/alert";
+import { Button } from "~/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "~/ui/card";
+import { useSearchParams } from "~/url/use-search-params";
+
+export function AccountDeletionForm({
+  lastResult,
+}: {
+  lastResult?: SubmissionResult;
+}) {
+  const { searchParams, withSearchParam, withoutSearchParam } =
+    useSearchParams();
+  const initialFormId = useId();
+  const [formId, setFormId] = useState(`form-${initialFormId}`);
+  const [form, fields] = useForm({
+    id: formId,
+    lastResult,
+    constraint: getValibotConstraint(accountDeletionSchema),
+    shouldRevalidate: "onInput",
+    onValidate({ formData }) {
+      return parseWithValibot(formData, { schema: accountDeletionSchema });
+    },
+  });
+
+  if (searchParams.has("delete-account")) {
+    return (
+      <FormProvider context={form.context}>
+        <Form
+          key={form.key}
+          method="post"
+          id={form.id}
+          aria-labelledby={`${form.id}-title`}
+          aria-describedby={form.errors != null ? form.errorId : undefined}
+          onSubmit={form.onSubmit}
+          className="mt-sm space-y-xs rounded-xl border bg-card p-sm text-card-foreground shadow"
+        >
+          <header className="flex items-center justify-between">
+            <CardTitle id={`${form.id}-title`}>Danger Zone</CardTitle>
+            <Button variant="ghost" size="icon" asChild>
+              <Link
+                to={withoutSearchParam("delete-account")}
+                replace
+                preventScrollReset
+                aria-label="Close"
+                onClick={() => {
+                  setFormId(`form-${uid()}`);
+                }}
+              >
+                <X />
+              </Link>
+            </Button>
+          </header>
+          <p>
+            <strong>Warning: Deleting your account cannot be undone</strong>
+          </p>
+          <p>
+            Enter the email associated with your account to permanently delete
+            your account
+          </p>
+          {form.errors != null && (
+            <Alert
+              variant="destructive"
+              id={form.errorId}
+              aria-labelledby={`${form.errorId}-heading`}
+            >
+              <AlertCircle className="size-xs" />
+              <AlertTitle level={4} id={`${form.errorId}-heading`}>
+                Error deleting account
+              </AlertTitle>
+              <AlertDescription>{form.errors[0]}</AlertDescription>
+            </Alert>
+          )}
+          <div hidden>
+            <AuthenticityTokenInput />
+          </div>
+          <FormField
+            name={fields.email.name}
+            render={({ field, data }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <Input
+                  {...field}
+                  type="email"
+                  autoComplete="email"
+                  defaultValue={data.initialValue}
+                />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <p>
+            <Button name="intent" value="delete-account" variant="secondary">
+              Delete account
+            </Button>
+          </p>
+        </Form>
+      </FormProvider>
+    );
+  }
+
+  return (
+    <Card aria-labelledby="danger-zone-heading" className="mt-sm">
+      <CardHeader>
+        <CardTitle id="danger-zone-heading" className="flex h-lg items-center">
+          Danger Zone
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>
+          <Button variant="destructive" asChild>
+            <Link
+              to={withSearchParam("delete-account")}
+              replace
+              preventScrollReset
+            >
+              Delete account
+            </Link>
+          </Button>
+        </p>
+      </CardContent>
+    </Card>
+  );
+}
