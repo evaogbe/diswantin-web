3d07a9622d7afa141dc77b312927ddab4cf4bb88
Eva Ogbe <eoogbe@gmail.com>
2024-12-03 14:42:00 -0500

    ✨ Add scheduled date/time

--- app/task/services.server.ts
+++ app/task/services.server.ts
@@ -1,4 +1,5 @@
-import { and, eq, sql } from "drizzle-orm";
+import { formatISO, startOfDay } from "date-fns";
+import { and, eq, isNotNull, isNull, lte, or, sql } from "drizzle-orm";
 import type { SQLChunk } from "drizzle-orm";
 import type { AnyPgColumn } from "drizzle-orm/pg-core";
 import { uid } from "uid";
@@ -11,11 +12,35 @@ function nullsLast(col: AnyPgColumn | SQLChunk) {
 }
 
 export async function getCurrentTask(userId: number) {
+  const now = new Date();
+  const today = formatISO(now, { representation: "date" });
+  const currentTime = formatISO(now, { representation: "time" });
+  const midnight = formatISO(startOfDay(now), { representation: "time" });
   const [currentTask] = await db
     .select({ id: table.task.clientId, name: table.task.name })
     .from(table.task)
-    .where(eq(table.task.userId, userId))
+    .where(
+      and(
+        eq(table.task.userId, userId),
+        or(
+          isNull(table.task.scheduledDate),
+          lte(table.task.scheduledDate, today),
+        ),
+        or(
+          isNull(table.task.scheduledTime),
+          lte(table.task.scheduledTime, currentTime),
+        ),
+      ),
+    )
     .orderBy(
+      nullsLast(table.task.scheduledDate),
+      nullsLast(
+        sql`CASE
+        WHEN ${isNotNull(table.task.scheduledTime)} THEN ${table.task.scheduledTime}
+        WHEN ${isNotNull(table.task.scheduledDate)} THEN ${midnight}
+        ELSE NULL
+        END`,
+      ),
       nullsLast(table.task.deadlineDate),
       nullsLast(table.task.deadlineTime),
       table.task.createdAt,
@@ -26,7 +51,18 @@ export async function getCurrentTask(userId: number) {
 }
 
 export function getNewTaskForm() {
-  return { id: uid(), name: "", deadlineDate: "", deadlineTime: "" };
+  return {
+    id: uid(),
+    name: "",
+    deadline: {
+      date: "",
+      time: "",
+    },
+    scheduledAt: {
+      date: "",
+      time: "",
+    },
+  };
 }
 
 export async function createTask(task: Task, userId: number) {
@@ -36,8 +72,10 @@ export async function createTask(task: Task, userId: number) {
       userId,
       clientId: task.id,
       name: task.name,
-      deadlineDate: task.deadlineDate,
-      deadlineTime: task.deadlineTime,
+      deadlineDate: task.deadline?.date,
+      deadlineTime: task.deadline?.time,
+      scheduledDate: task.scheduledAt?.date,
+      scheduledTime: task.scheduledAt?.time,
     })
     .onConflictDoNothing({ target: table.task.clientId });
 }
