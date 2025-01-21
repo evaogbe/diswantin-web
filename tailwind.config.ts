9075fb6e2e4196b9d14efcf297ddf839b9d1958d
Eva Ogbe <eoogbe@gmail.com>
2025-01-20 20:07:49 -0500

    💄 Convert border radius to pixels (#175)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,9 +33,9 @@ export default {
     },
     extend: {
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        sm: "var(--radius)",
+        md: "calc(var(--radius) + 2px)",
+        lg: "calc(var(--radius) + 4px)",
       },
       colors: {
         background: "hsl(var(--background))",
